affc0c5ad76486e2bf1b5561eaa0e66d0bbc31f4
SpiderGamin <jaytaygame@gmail.com>
2020-05-29 20:17:49 -0700

    Um

---
+++ README.md
@@ -0,0 +1,21 @@
+# SpaceCompany Desktop
+Im making a desktop version of a game called <a href="https://github.com/sparticle999/spacecompany">Space Company</a> made by <a href="https://github.com/sparticle999">Sparticle999</a> on github
+
+## Overview:
+Space Company is a science-fiction incremental game where you start from humble beginnings on Earth, working your way up to travelling between star systems and building Dyson Spheres and pretty much colonising the entire galaxy(ies?)
+
+
+## Plans
+* Redesign the ui (still using bootstrap) to make it feel more like a desktop program.
+* Waiting for version 1 of <a href="https://github.com/sparticle999/spacecompany">Space Company</a> to be released befor I do too much work on this project
+
+### How To Install The Game
+As of now, you can run the raw code using `npm start` in the code directory
+
+I have only made a linux build of the game, the installer can be found <a href="">here</a>
+
+I will make windows and MacOS build soon, dont worry. But if you really need to play it on your platform (what I did at least), 
+Open up the terminal and `cd` into the game code directory.
+Then run: 
+```mpm run make```
+This should build the code for your OS. Once that is done, open up the `out` directory that was made
---
+++ src/index.js
@@ -0,0 +1,98 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+    app.quit();
+}
+
+const createWindow = () => {
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 1250,
+        height: 700,
+        icon: path.join(__dirname, 'SpaceCompanyFavicon.png'),
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    const template = [
+        {
+            label: 'File',
+            submenu: [
+                {
+                    role: 'reload'
+                },
+                {
+                    role: 'toggleFullscreen'
+                },
+                {
+                    label: 'Save',
+                    click() {
+
+                    }
+                }
+            ]
+        },
+        {
+            label: 'Edit',
+            submenu: [
+                {
+                    role: 'undo'
+                },
+                {
+                    role: 'redo'
+                }
+            ]
+        },
+        {
+            label: 'Game',
+            submenu: [
+                {
+                    label: 'Home',
+                    click() {
+                        mainWindow.webContents.loadFile(path.join(__dirname, 'index.html'))
+                    }
+                },
+                {
+                    label: 'SpaceCompany Source',
+                    click() {
+                        mainWindow.webContents.loadURL(`https://github.com/sparticle999/spacecompany`)
+                    }
+                }
+            ]
+        }
+    ]
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
+    // and load the index.html of the app.
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+    // Open the DevTools.
+    //mainWindow.webContents.openDevTools();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.

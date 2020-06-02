da6043a9db1d2aa8cb7656371625fdcaae55dc44
SpiderGamin <jaytaygame@gmail.com>
2020-06-02 00:32:32 -0700

    Worked on the menu bar a little

--- src/spacecompany/game/js-style/style.js
+++ src/spacecompany/game/js-style/style.js
@@ -1,12 +1,32 @@
 // No Content Yet Cause Its BedTime
-var app = require('electron').remote; 
+var app = require('electron').remote;
 
 // Close app 
+var cloased;
+cloased = true;
+document.getElementById("Window").addEventListener('click', () => {
+    if (cloased) {
+        cloased = false
+        document.getElementById('Window-Menu').style.display = 'inline';
+    } else {
+        cloased = true
+        document.getElementById('Window-Menu').style.display = 'none';
+    }
+    
+})
+
+
+document.getElementById("bar-title").innerHTML = 'hi';
+
 document.getElementById("close-btn").addEventListener("click", () => {
     app.BrowserWindow.getFocusedWindow().close();
 }, false);
 
 // Close app 
-document.getElementById("devtools").addEventListener("click", () => {
-    app.BrowserWindow.getFocusedWindow().openDevTools();
+document.getElementById("max-btn").addEventListener("click", () => {
+    app.BrowserWindow.getFocusedWindow().maximize();
+}, false);
+
+document.getElementById("min-btn").addEventListener("click", () => {
+    app.BrowserWindow.getFocusedWindow().minimize();
 }, false);
